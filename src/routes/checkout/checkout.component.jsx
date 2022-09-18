7740c58e0b90d6a89ab56421fb7080b882d0e1c2
Jhon <asayj87@gmail.com>
2022-09-18 12:05:20 -1000

    add addToCart feature

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,11 @@
+import './checkout.styles.scss';
+
+const Checkout = () => {
+  return (
+    <div className="checkout-container">
+      <h1>Checkout</h1>
+    </div>
+  );
+};
+
+export default Checkout;
